1284920b3ce98ffb7da8d16b7a20d41e3f08a789
son-inchang <3721cha21.11@gmail.com>
2024-03-07 15:43:22 +0900

    feat #3: 프로필기능 관련 DB 테이블 변경사항 적용 - script 초기화 관련 property 추가 - db 설정 관련 dependency 추가 - DB 예약어 배제(user, token, name) - Users 테이블 생성 - 논리삭제 관련 is_active 생성 - 재직여부 data type boolean 으로 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,20 +1,28 @@
-insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (1, 'Raymond', 'Dod', 'rdod0@moonfruit.com', 'Male', '132.69.45.81');
-insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (2, 'Arlana', 'McCarrell', 'amccarrell1@eepurl.com', 'Polygender', '191.76.90.207');
-insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (3, 'Cornelle', 'Tire', 'ctire2@imageshack.us', 'Female', '6.203.142.233');
-insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (4, 'Anderea', 'Chasmer', 'achasmer3@tiny.cc', 'Female', '135.117.99.43');
-insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (5, 'Simmonds', 'Press', 'spress4@histats.com', 'Male', '114.167.194.123');
-insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (6, 'Pansy', 'Daouse', 'pdaouse5@craigslist.org', 'Female', '126.204.97.217');
-insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (7, 'Domeniga', 'Toffano', 'dtoffano6@joomla.org', 'Female', '171.214.226.199');
-insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (8, 'Holly', 'Vowells', 'hvowells7@a8.net', 'Female', '193.104.116.186');
-insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (9, 'Abbie', 'Lievesley', 'alievesley8@shinystat.com', 'Male', '124.64.117.88');
-insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (10, 'Reuben', 'Bird', 'rbird9@answers.com', 'Male', '17.189.46.54');
-insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (11, 'Jessa', 'Foulsham', 'jfoulshama@buzzfeed.com', 'Female', '234.125.193.207');
-insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (12, 'Benedetta', 'Rudsdell', 'brudsdellb@vk.com', 'Female', '27.82.94.112');
-insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (13, 'Darelle', 'Malone', 'dmalonec@typepad.com', 'Female', '32.97.100.65');
-insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (14, 'Danny', 'Warde', 'dwarded@yelp.com', 'Male', '248.188.24.163');
-insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (15, 'Nissy', 'Fleeman', 'nfleemane@auda.org.au', 'Female', '0.56.17.194');
-insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (16, 'Jedediah', 'Fautly', 'jfautlyf@edublogs.org', 'Male', '132.248.84.206');
-insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (17, 'Thibaut', 'Giffen', 'tgiffeng@issuu.com', 'Male', '43.210.17.168');
-insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (18, 'Fidel', 'Oxlade', 'foxladeh@nifty.com', 'Male', '234.244.155.61');
-insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (19, 'Rikki', 'Hannigan', 'rhannigani@4shared.com', 'Female', '87.141.2.39');
-insert into MOCK_DATA (id, first_name, last_name, email, gender, ip_address) values (20, 'Kele', 'Sawle', 'ksawlej@phpbb.com', 'Genderqueer', '77.214.249.22');
+insert into users(user_id, user_name, phone_number, user_email)
+values (1, '테스트', '010-1111-2222', 'test01@naver.com');
+
+insert into Account(account_id, user_id, pw_hash, last_login_dt)
+values ('test01@naver.com', 1, '401357cf18542b4117ca59800657b64cce2a36d8ad4c56b6102a1e0b03049e97', now());
+
+insert into Consent(consent_id, user_id, marketing_consent, terms_conditions_consent)
+values (1, 1, true, true);
+
+insert into Profile(profile_id, user_id, title, profile_job_opt)
+values (1, 1, '테스트 데이터 입니다.', 1);
+
+insert into Profile_Count(profile_count_id, profile_id)
+values (1, 1);
+
+insert into Company(company_id, profile_id, company_nm, company_position, current_state, start_date)
+values (1, 1, 'test.corp', 'manager', 1, now());
+
+insert into Education(edu_id, profile_id, institute, major, current_state, start_date)
+values (1, 1, 'test_univ', 'computer-science', 1, now());
+
+insert into Skill(skill_id, skill_name, category, second_category)
+values(1, 'spring web', 'development', 'back-end');
+insert into Skill(skill_id, skill_name, category, second_category)
+values(2, 'spring cloud', 'development', 'back-end');
+
+insert into Profile_Skill(profile_id, skill_id)
+values (1, 1), (1, 2);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,39 +1,61 @@
+DROP SEQUENCE IF EXISTS user_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS profile_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS profile_count_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS login_key_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS consent_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS company_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS edu_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS profile_sites_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS skill_id_seq CASCADE;
+
+-- Create SEQUENCE for Users.user_id
+CREATE SEQUENCE IF NOT EXISTS user_id_seq START 100;
 -- Create SEQUENCE for Profile.profile_id
-CREATE SEQUENCE IF NOT EXISTS profile_id_seq;
+CREATE SEQUENCE IF NOT EXISTS profile_id_seq START 100;
 -- Create SEQUENCE for Profile_count.profile_count_id
-CREATE SEQUENCE IF NOT EXISTS profile_count_id_seq;
--- Create SEQUENCE for Token.token_id
-CREATE SEQUENCE IF NOT EXISTS token_id_seq;
+CREATE SEQUENCE IF NOT EXISTS profile_count_id_seq START 100;
+-- Create SEQUENCE for Login_Key.login_key_id
+CREATE SEQUENCE IF NOT EXISTS login_key_id_seq START 100;
 -- Create SEQUENCE for Consent.consent_id
-CREATE SEQUENCE IF NOT EXISTS consent_id_seq;
+CREATE SEQUENCE IF NOT EXISTS consent_id_seq START 100;
 -- Create SEQUENCE for Company.company_id
-CREATE SEQUENCE IF NOT EXISTS company_id_seq;
+CREATE SEQUENCE IF NOT EXISTS company_id_seq START 100;
 -- Create SEQUENCE for Education.edu_id
-CREATE SEQUENCE IF NOT EXISTS edu_id_seq;
+CREATE SEQUENCE IF NOT EXISTS edu_id_seq START 100;
 -- Create SEQUENCE for Profile_sites.profile_sites_id
-CREATE SEQUENCE IF NOT EXISTS profile_sites_id_seq;
+CREATE SEQUENCE IF NOT EXISTS profile_sites_id_seq START 100;
 -- Create SEQUENCE for Skill.skill_id
-CREATE SEQUENCE IF NOT EXISTS skill_id_seq;
+CREATE SEQUENCE IF NOT EXISTS skill_id_seq START 100;
 
-
--- Create Account table
-CREATE TABLE IF NOT EXISTS Account (
-                                       account_email_id VARCHAR PRIMARY KEY,
-                                       pw_hash VARCHAR NOT NULL,
-                                       last_login_dt TIMESTAMP WITH TIME ZONE,
-                                       create_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                                       update_at TIMESTAMP WITH TIME ZONE
+DROP TABLE IF EXISTS Users CASCADE;
+CREATE table IF not EXISTS Users (
+    user_id int primary key default nextval('user_id_seq'),
+    user_name varchar not null,
+    phone_number varchar not null,
+    user_email varchar not null,
+    is_active BOOLEAN DEFAULT TRUE
 );
 
 
+-- Create Account table
+DROP TABLE IF EXISTS Account CASCADE;
+CREATE TABLE IF NOT EXISTS Account (
+    account_id varchar primary key,
+    user_id int references Users(user_id),
+    pw_hash VARCHAR NOT NULL,
+    last_login_dt TIMESTAMP WITH TIME ZONE,
+    create_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_at TIMESTAMP WITH TIME ZONE,
+    is_active BOOLEAN DEFAULT TRUE
+);
+
 -- Create Profile table
+DROP TABLE IF EXISTS Profile CASCADE;
 CREATE TABLE IF NOT EXISTS Profile (
     profile_id INT PRIMARY KEY DEFAULT nextval('profile_id_seq'),
-    account_email_id VARCHAR REFERENCES Account(account_email_id),
-    name VARCHAR NOT NULL,
+    user_id int references Users(user_id),
     image_url VARCHAR,
     title VARCHAR NOT NULL,
-    phone_number VARCHAR NOT NULL,
     short_description VARCHAR,
     description VARCHAR,
     links VARCHAR,
@@ -42,10 +64,12 @@ CREATE TABLE IF NOT EXISTS Profile (
     profile_job_opt INT NOT NULL,
     openToSearchEngine INT NOT NULL DEFAULT 0,
     create_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    update_at TIMESTAMP WITH TIME ZONE
-                            );
+    update_at TIMESTAMP WITH TIME ZONE,
+    is_active BOOLEAN DEFAULT TRUE
+);
 
 -- Create Profile_count
+DROP TABLE IF EXISTS Profile_Count CASCADE;
 CREATE TABLE IF NOT EXISTS Profile_count (
     profile_count_id INT PRIMARY KEY DEFAULT nextval('profile_count_id_seq'),
     profile_id INT REFERENCES Profile(profile_id),
@@ -54,88 +78,100 @@ CREATE TABLE IF NOT EXISTS Profile_count (
     post_count INT NOT NULL DEFAULT 0,
     create_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     update_at TIMESTAMP WITH TIME ZONE
-                            );
+);
 
--- Create Token table
-CREATE TABLE IF NOT EXISTS Token (
-    token_id INT PRIMARY KEY DEFAULT nextval('token_id_seq'),
-    profile_id INT REFERENCES Profile(profile_id),
-    access_token VARCHAR UNIQUE,
-    refresh_token VARCHAR UNIQUE,
+-- Create Login_Key table
+DROP TABLE IF EXISTS Login_Key CASCADE;
+CREATE TABLE IF NOT EXISTS Login_Key (
+    login_key_id INT PRIMARY KEY DEFAULT nextval('login_key_id_seq'),
+    user_id int references Users(user_id),
+    access_token VARCHAR UNIQUE NOT NULL,
+    refresh_token VARCHAR UNIQUE NOT NULL,
     expire_at TIMESTAMP WITH TIME ZONE NOT NULL,
     create_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
-                            );
+);
 
 -- Create Consent table
+DROP TABLE IF EXISTS Consent CASCADE;
 CREATE TABLE IF NOT EXISTS Consent (
     consent_id INT PRIMARY KEY DEFAULT nextval('consent_id_seq'),
-    profile_id INT REFERENCES Profile(profile_id),
+    user_id int references Users(user_id),
     marketing_consent BOOLEAN NOT NULL,
     terms_conditions_consent BOOLEAN NOT NULL,
     create_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     update_at TIMESTAMP WITH TIME ZONE
-                            );
+);
 
 -- Create Company table
+DROP TABLE IF EXISTS Company CASCADE;
 CREATE TABLE IF NOT EXISTS Company (
     company_id INT PRIMARY KEY DEFAULT nextval('company_id_seq'),
     profile_id INT REFERENCES Profile(profile_id),
     company_nm VARCHAR NOT NULL,
     company_position VARCHAR NOT NULL,
     description VARCHAR,
-    is_current INT NOT NULL,
+    current_state INT NOT NULL,
     url VARCHAR,
     start_date TIMESTAMP WITH TIME ZONE NOT NULL,
     end_date TIMESTAMP WITH TIME ZONE,
     create_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    update_at TIMESTAMP WITH TIME ZONE
-                            );
+    update_at TIMESTAMP WITH TIME ZONE,
+    is_active BOOLEAN DEFAULT TRUE
+);
 
 -- Create Education table
+DROP TABLE IF EXISTS Education CASCADE;
 CREATE TABLE IF NOT EXISTS Education (
     edu_id INT PRIMARY KEY DEFAULT nextval('edu_id_seq'),
     profile_id INT REFERENCES Profile(profile_id),
     institute VARCHAR NOT NULL,
     major VARCHAR NOT NULL,
     description VARCHAR,
-    is_current INT NOT NULL,
+    current_state INTEGER NOT NULL,
     url VARCHAR,
     start_date TIMESTAMP WITH TIME ZONE NOT NULL,
     end_date TIMESTAMP WITH TIME ZONE,
     create_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    update_at TIMESTAMP WITH TIME ZONE
-                            );
+    update_at TIMESTAMP WITH TIME ZONE,
+    is_active BOOLEAN DEFAULT TRUE
+);
 
 -- Create Profile_sites table
+DROP TABLE IF EXISTS Profile_sites CASCADE;
 CREATE TABLE IF NOT EXISTS Profile_sites (
     profile_sites_id INT PRIMARY KEY DEFAULT nextval('profile_sites_id_seq'),
     profile_id INT REFERENCES Profile(profile_id),
     create_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    update_at TIMESTAMP WITH TIME ZONE
-                            );
+    update_at TIMESTAMP WITH TIME ZONE,
+    is_active BOOLEAN DEFAULT TRUE
+);
 
 -- Create Skill table
+DROP TABLE IF EXISTS Skill CASCADE;
 CREATE TABLE IF NOT EXISTS Skill (
     skill_id INT PRIMARY KEY DEFAULT nextval('skill_id_seq'),
-    name VARCHAR NOT NULL,
+    skill_name VARCHAR NOT NULL,
     category VARCHAR NOT NULL,
     second_category VARCHAR NOT NULL,
     create_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     update_at TIMESTAMP WITH TIME ZONE
-                            );
+);
 
 -- Create Profile_skill table
+DROP TABLE IF EXISTS Profile_Skill CASCADE;
 CREATE TABLE IF NOT EXISTS Profile_skill (
     profile_id INT REFERENCES Profile(profile_id),
     skill_id INT REFERENCES Skill(skill_id),
     create_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     update_at TIMESTAMP WITH TIME zone,
-                            primary key(profile_id, skill_id)
-    );
+    is_active BOOLEAN DEFAULT TRUE,
+    PRIMARY KEY(profile_id, skill_id)
+);
 
+ALTER SEQUENCE user_id_seq OWNED BY Users.user_id;
 ALTER SEQUENCE profile_id_seq OWNED BY Profile.profile_id;
 ALTER SEQUENCE profile_count_id_seq OWNED BY Profile_count.profile_count_id;
-ALTER SEQUENCE token_id_seq OWNED BY Token.token_id;
+ALTER SEQUENCE login_key_id_seq OWNED BY Login_Key.login_key_id;
 ALTER SEQUENCE consent_id_seq OWNED BY Consent.consent_id;
 ALTER SEQUENCE company_id_seq OWNED BY Company.company_id;
 ALTER SEQUENCE edu_id_seq OWNED BY Education.edu_id;
